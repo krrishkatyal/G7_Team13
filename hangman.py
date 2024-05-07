0d96a640b6ddf06ada85e5f4d9b49db43378187b
krrishkatyal <krrish0553.be23@chitkara.edu.in>
2024-05-07 09:35:55 +0530

    Hangman version 2

--- hangman.py
+++ hangman.py
@@ -4,34 +4,38 @@ import random
 pygame.init()
 winHeight = 480
 winWidth = 700
-win=pygame.display.set_mode((winWidth,winHeight))
+win = pygame.display.set_mode((winWidth, winHeight))
 
-BLACK = (0,0, 0)
-WHITE = (255,255,255)
-RED = (255,0, 0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
-LIGHT_BLUE = (102,255,255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+LIGHT_BLUE = (102, 255, 255)
 YELLOW = (255, 255, 0)
 
 btn_font = pygame.font.SysFont("arial", 20)
 guess_font = pygame.font.SysFont("monospace", 24)
 lost_font = pygame.font.SysFont('arial', 45)
-hint_font = pygame.font.SysFont('arial', 30)  
+hint_font = pygame.font.SysFont('arial', 30)
 word = ''
 buttons = []
 guessed = []
-hangmanPics = [pygame.image.load('hangman0.png'), pygame.image.load('hangman1.png'), pygame.image.load('hangman2.png'), pygame.image.load('hangman3.png'), pygame.image.load('hangman4.png'), pygame.image.load('hangman5.png'), pygame.image.load('hangman6.png')]
+hangmanPics = [pygame.image.load('hangman0.png'), pygame.image.load('hangman1.png'),
+               pygame.image.load('hangman2.png'), pygame.image.load('hangman3.png'),
+               pygame.image.load('hangman4.png'), pygame.image.load('hangman5.png'),
+               pygame.image.load('hangman6.png')]
 
 limbs = 0
-hint = ''  
+hint = ''
+
 
 def redraw_game_window():
     global guessed
     global hangmanPics
     global limbs
-    global hint  
-    win.fill( WHITE)
+    global hint
+    win.fill(WHITE)
     for i in range(len(buttons)):
         if buttons[i][4]:
             pygame.draw.circle(win, BLACK, (buttons[i][1], buttons[i][2]), buttons[i][3])
@@ -43,26 +47,27 @@ def redraw_game_window():
     label1 = guess_font.render(spaced, 1, BLACK)
     rect = label1.get_rect()
     length = rect[2]
-    
-    win.blit(label1,(winWidth/2 - length/2, 400))
+
+    win.blit(label1, (winWidth / 2 - length / 2, 400))
 
     pic = hangmanPics[limbs]
-    win.blit(pic, (winWidth/2 - pic.get_width()/2 + 20, 150))
+    win.blit(pic, (winWidth / 2 - pic.get_width() / 2 + 20, 150))
 
     # Display the hint
     hint_label = hint_font.render("Hint: " + hint, 1, BLACK)
-    win.blit(hint_label, (20, 150))  
+    win.blit(hint_label, (20, 150))
 
     pygame.display.update()
+
 
 def randomWord():
     file = open('words.txt')
     f = file.readlines()
     i = random.randrange(0, len(f) - 1)
 
-    
     word_hint = f[i][:-1].split(',')
     return word_hint[0], word_hint[1]
+
 
 def hang(guess):
     global word
@@ -70,6 +75,7 @@ def hang(guess):
         return True
     else:
         return False
+
 
 def spacedOut(word, guessed=[]):
     spacedWord = ''
@@ -85,25 +91,26 @@ def spacedOut(word, guessed=[]):
             spacedWord += ' '
     return spacedWord
 
+
 def buttonHit(x, y):
     for i in range(len(buttons)):
         if x < buttons[i][1] + 20 and x > buttons[i][1] - 20:
             if y < buttons[i][2] + 20 and y > buttons[i][2] - 20:
-                return buttons[i][5]
+                return i
     return None
+
 
 def end(winner=False):
     global limbs
     lostTxt = 'You Lost, press any key to play again...'
     winTxt = 'WINNER!, press any key to play again...'
-    
+
     redraw_game_window()
     pygame.time.delay(500)
-    
 
-    if winner == True:
+    if winner:
         label = lost_font.render(winTxt, 1, BLACK)
-        win.fill(GREEN) 
+        win.fill(GREEN)
         wordTxt = lost_font.render(word.upper(), 1, BLACK)
         wordWas = lost_font.render('The word was: ', 1, BLACK)
     else:
@@ -112,8 +119,8 @@ def end(winner=False):
         wordTxt = lost_font.render(word.upper(), 1, WHITE)
         wordWas = lost_font.render('The word was: ', 1, WHITE)
 
-    win.blit(wordTxt, (winWidth/2 - wordTxt.get_width()/2, 295))
-    win.blit(wordWas, (winWidth/2 - wordWas.get_width()/2, 245))
+    win.blit(wordTxt, (winWidth / 2 - wordTxt.get_width() / 2, 295))
+    win.blit(wordWas, (winWidth / 2 - wordWas.get_width() / 2, 245))
     win.blit(label, (winWidth / 2 - label.get_width() / 2, 140))
     pygame.display.update()
     again = True
@@ -124,6 +131,7 @@ def end(winner=False):
             if event.type == pygame.KEYDOWN:
                 again = False
     reset()
+
 
 def reset():
     global limbs
@@ -137,6 +145,7 @@ def reset():
     limbs = 0
     guessed = []
     word, hint = randomWord()  # Update this line
+
 
 increase = round(winWidth / 13)
 for i in range(26):
@@ -161,20 +170,37 @@ while inPlay:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 inPlay = False
+            elif pygame.K_a <= event.key <= pygame.K_z:
+                letter = chr(event.key).upper()
+                if letter not in guessed:
+                    guessed.append(letter)
+                    button_index = ord(letter) - 65
+                    buttons[button_index][4] = False
+                    if hang(letter):
+                        if limbs != 6:
+                            limbs += 1
+                        else:
+                            end()
+                    else:
+                        print(spacedOut(word, guessed))
+                        if spacedOut(word, guessed).count('_') == 0:
+                            end(True)
         if event.type == pygame.MOUSEBUTTONDOWN:
             clickPos = pygame.mouse.get_pos()
-            letter = buttonHit(clickPos[0], clickPos[1])
-            if letter != None:
-                guessed.append(chr(letter))
-                buttons[letter - 65][4] = False
-                if hang(chr(letter)):
-                    if limbs != 6:
-                        limbs += 1
+            button_index = buttonHit(clickPos[0], clickPos[1])
+            if button_index is not None:
+                letter = chr(buttons[button_index][5])
+                if letter not in guessed:
+                    guessed.append(letter)
+                    buttons[button_index][4] = False
+                    if hang(letter):
+                        if limbs != 6:
+                            limbs += 1
+                        else:
+                            end()
                     else:
-                        end()
-                else:
-                    print(spacedOut(word, guessed))
-                    if spacedOut(word, guessed).count('_') == 0:
-                        end(True)
+                        print(spacedOut(word, guessed))
+                        if spacedOut(word, guessed).count('_') == 0:
+                            end(True)
 
 pygame.quit()
